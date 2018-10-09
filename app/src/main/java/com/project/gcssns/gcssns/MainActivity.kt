29ce9208204a24ce20d5c1409f6093b82d5dd9eb
Leehwasub <hwasub1115@naver.com>
2018-10-09 17:56:56 +0900

    메인 메뉴 기본 툴바 추가

--- app/src/main/java/com/project/gcssns/gcssns/MainActivity.kt
+++ app/src/main/java/com/project/gcssns/gcssns/MainActivity.kt
@@ -2,11 +2,14 @@ package com.project.gcssns.gcssns
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
     }
 }
