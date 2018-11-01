fa7495cd1f6ea74063d5263cb6d59a78248134cf
leehwasub <hwasub1115@naver.com>
2018-11-01 14:42:17 +0900

    이미지 선택 업로드 추가(테스트 필요), 이제 회원가입시 user정보 데이터베이스에 넣을수 있음

--- app/src/main/java/com/project/gcssns/gcssns/SignupActivity.kt
+++ app/src/main/java/com/project/gcssns/gcssns/SignupActivity.kt
@@ -1,11 +1,21 @@
 package com.project.gcssns.gcssns
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.project.gcssns.gcssns.model.User
 import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
 
 class SignupActivity : AppCompatActivity() {
 
@@ -22,6 +32,12 @@ class SignupActivity : AppCompatActivity() {
         button_signup_submit.setOnClickListener {
             signup()
         }
+
+        button_signup_photo.setOnClickListener {
+            var intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
     }
 
     fun signup(){
@@ -35,10 +51,46 @@ class SignupActivity : AppCompatActivity() {
         auth!!.createUserWithEmailAndPassword(editText_signup_email.text.toString(), editText_signup_password.text.toString()).addOnCompleteListener {task ->
             if(task.isSuccessful){
                 Toast.makeText(this,"회원가입에 성공했습니다.", Toast.LENGTH_LONG).show()
+                uploadImage()
                 finish()
             } else{
                 Toast.makeText(this, task.exception.toString(), Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    fun uploadImage(){
+        if(selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+                        Log.d("SignupActivity", "File Location : $it")
+                        saveUserDataToDatabase(selectedPhotoUri.toString())
+                    }
+                }
+    }
+
+    fun saveUserDataToDatabase(selectedPhotoUri: String){
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = User(uid, selectedPhotoUri, editText_signup_email.text.toString(), editText_signup_name.text.toString(), editText_signup_studentId.text.toString(), editText_signup_major.text.toString())
+        ref.setValue(user)
+                .addOnSuccessListener {
+                    Log.d("SignupActivity", "User Information saved")
+                }
+    }
+
+    var selectedPhotoUri : Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            val bitamapDrawable = BitmapDrawable(bitmap)
+            button_signup_photo.setBackgroundDrawable(bitamapDrawable)
         }
     }
 }
--- app/src/main/java/com/project/gcssns/gcssns/model/User.kt
+++ app/src/main/java/com/project/gcssns/gcssns/model/User.kt
@@ -4,4 +4,4 @@ package com.project.gcssns.gcssns.model
  * Created by i412 on 2018-10-02.
  */
 
-data class User(var userEmail : String? = null, var userName : String? = null, var userStudentId : String? = null, var userMajor : String? = null)
+data class User(var uid: String? = null, var profileImageUrl: String? = null, var userEmail : String? = null, var userName : String? = null, var userStudentId : String? = null, var userMajor : String? = null)
